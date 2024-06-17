999012f9e75ee55c739a6cb743110d7b2b31c434
Yuhno Alexandra <a.yuhno@tg.dunice.net>
2024-06-17 15:06:49 +0300

    Task Add

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const dom ={
-    new : document.getElementById('new'),
-    add : document.getElementById('add')
-}
+const newInput = document.getElementById('newInput');
+const btnAdd = document.getElementById('btnAdd');
+const task = document.getElementById('task')
+const taskList =document.getElementById('taskList')
+
+let todoList = []
 
 //Track click button 'Add'
 
-dom.add.onclick=()=>{
-        addTask()
+
+render = () => {
+    let taskHTML = ""
+    const taskText = newInput.value 
+    todoList.forEach((taskHTML) => {
+    taskHTML += ` <li class="todo__task" id="task">
+                        <label class="todo__checkbox"> 
+                             <input type="checkbox">
+                        </label>
+                        <div class="todo__task-title"> ${taskText}</div>
+                     </li>`   });
+ taskList.innerHTML = taskHTML
+
 }
 
-function addTask(){
-    alert("ok")
-}
+btnAdd.addEventListener("click", function addTask(){
+    let newTodo = {
+        text: newInput.value,
+        isCompleted:false,
+        };
+    todoList.push(newTodo);
+    
+    render();
+});
+
+
