d027ee06a8e4c43232e0e64db0f10e35f4a3f527
Yuhno Alexandra <a.yuhno@tg.dunice.net>
2024-06-17 17:47:59 +0300

    Corrected JS

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 const newInput = document.getElementById('newInput');
 const btnAdd = document.getElementById('btnAdd');
-const task = document.getElementById('task')
-const taskList =document.getElementById('taskList')
+const taskList =document.getElementById('taskList');
 
-let todoList = []
+let todoList = [];
 
 //Track click button 'Add'
 
 
-render = () => {
-    let taskHTML = ""
-    const taskText = newInput.value 
-    todoList.forEach((taskHTML) => {
-    taskHTML += ` <li class="todo__task" id="task">
+function showTasks(){ 
+    
+    let taskHTML =""
+    todoList.forEach(function(item, i){
+      taskHTML += ` <li class="todo__task">
                         <label class="todo__checkbox"> 
-                             <input type="checkbox">
-                        </label>
-                        <div class="todo__task-title"> ${taskText}</div>
-                     </li>`   }
- todoList.innerHTML = taskHTML
-);
+                             <input type="checkbox" id= "item_${i}">
+                        </label for = 'id= "item_${i}">
+                        <div class="todo__task-title"> ${item.text}</div>
+                     </li>`   ;
+        taskList.innerHTML = taskHTML;
+    }
+)}
 
-}
 
 btnAdd.addEventListener("click", function addTask(){
     let newTodo = {
@@ -29,8 +28,5 @@ btnAdd.addEventListener("click", function addTask(){
         isCompleted:false,
         };
     todoList.push(newTodo);
-    
-    render();
+    showTasks()
 });
-
-
